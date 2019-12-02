e0779ead3d0503bf642277bcba714b992387c7e6
jermaldo <jermaldo@iu.edu>
2019-12-02 16:47:54 -0500

    Adds Gravity and movement to player

--- Godot/Scripts/Player.gd
+++ Godot/Scripts/Player.gd
@@ -2,14 +2,20 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 
-
+const up = Vector2(0, -1)
+const gravity = 20
 const speed = 150
+const jump_speed = 500
 
 func _physics_process(delta):
+	velocity.y += gravity
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	elif Input.is_action_pressed("move_left"):
-		velocity.x = speed
+		velocity.x = -speed
 	else:
 		velocity.x = 0
-	velocity = move_and_slide(velocity) 
+	if is_on_floor():
+		if Input.is_action_pressed("jump"):
+			velocity.y = -jump_speed
+	velocity = move_and_slide(velocity, up) 
