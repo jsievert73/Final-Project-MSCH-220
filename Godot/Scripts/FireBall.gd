b46c2eac2f2bd27a37fb0cec461c80f440538b00
brywhitm <brywhitm@iu.edu>
2019-12-03 19:16:43 -0500

    update

---
+++ Godot/Scripts/FireBall.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var speed = 200
+var velocity = Vector2()
+var direction = 1
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	$AnimatedSprite.play("Shoot")
+	velocity.x = speed*delta*direction
+	translate(velocity)
+
+func fireball_direction(dir):
+	direction = dir
+	if dir == -1:
+		$AnimatedSprite.flip_h = true
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
