b58940ce2b9851dab84088b0683d2bb545a35794
jermaldo <jermaldo@iu.edu>
2019-12-04 10:17:21 -0500

    Adds simple goal

---
+++ Godot/Scripts/Goal.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var next_scene
+
+func _ready():
+	pass
+
+func _on_Goal_body_entered(body):
+	if "Player" in body.name:
+		get_tree().change_scene(next_scene)
